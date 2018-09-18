1bca4dee1baba210802b38d49e3cde68d16ab7c8
katerinagl <ekaterina@roundforest.com>
2018-09-18 12:28:58 +0300

    clean up

--- src/main/scala/LightGbmClassifierDemo.scala
+++ src/main/scala/LightGbmClassifierDemo.scala
@@ -1,11 +1,9 @@
-import Titanic.{accuracyScore, evaluateResult, handleCategorical}
-import com.microsoft.ml.spark.{LightGBMClassificationModel, LightGBMClassifier, LightGBMRegressionModel, LightGBMRegressor}
-import ml.dmlc.xgboost4j.scala.spark.{XGBoost, XGBoostModel}
+import com.microsoft.ml.spark.{LightGBMRegressionModel, LightGBMRegressor}
 import org.apache.spark.SparkConf
-import org.apache.spark.ml.{Pipeline, PipelineStage, PredictionModel}
-import org.apache.spark.ml.feature.{OneHotEncoderEstimator, StringIndexer, VectorAssembler}
+import org.apache.spark.ml.{PredictionModel}
+import org.apache.spark.ml.feature.{StringIndexer}
+import org.apache.spark.mllib.evaluation.RegressionMetrics
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.sql.functions._
 
 object LightGbmClassifierDemo {
   def main(args: Array[String]): Unit = {
@@ -15,20 +13,17 @@ object LightGbmClassifierDemo {
     //    val df = sparkSession.read
     //      .option("header", "true")
     //      .option("inferSchema", "true")
-    //      //      .csv("src/main/resources/air_small.csv")
     //      .csv("src/main/resources/2008_14col.csv")
     //
     //    df.show()
     //
     //    val Array(trainDf, testDf) = df
-    //      .withColumnRenamed("ArrDelay", "label")
-    //      //      .withColumn("label", when(col("ArrDelay") < 0, 0).otherwise(1))
+    //      .withColumn("label", when(col("ArrDelay") < 0, 0).otherwise(1))
     //      .randomSplit(Array(0.001, 0.001))
     //
     //
     //    val categoricalFeaturesStages = handleCategorical(Array("UniqueCarrier", "Origin", "Dest"))
-    //
-    //    //    val cols = Array("Year","Month","DayofMonth","DayofWeek","CRSDepTime","CRSArrTime","FlightNum","ActualElapsedTime","Distance","Diverted")
+
     //    val cols = Array("Year", "Month", "DayofMonth", "DayofWeek", "CRSDepTime", "CRSArrTime", "UniqueCarrier_index",
     //      "FlightNum", "ActualElapsedTime", "Origin_index", "Dest_index", "Distance", "Diverted")
     //    val vectorAssembler = new VectorAssembler().setInputCols(cols).setOutputCol("features")
@@ -39,8 +34,7 @@ object LightGbmClassifierDemo {
     //    val testData = new Pipeline().setStages(preProcessStages).fit(testDf).transform(trainDf)
     //    trainData.write.parquet("src/main/resources/2008_14col_train")
 
-    //    val trainData = sparkSession.read.parquet("/Users/katerinaglushchenko/Downloads/HIGGS_train")//.coalesce(1)
-    val trainData = sparkSession.read.parquet("src/main/resources/2008_14col_train") //.coalesce(1)
+    val trainData = sparkSession.read.parquet("src/main/resources/2008_14col_train")
     println("TOTAL: " + trainData.count())
     println("PARTITIONS : " + trainData.rdd.partitions.length)
 
@@ -66,8 +60,13 @@ object LightGbmClassifierDemo {
         .setHandleInvalid("skip")
     }
 
-    //    val oneHot = new OneHotEncoderEstimator()
-    //      .setInputCols(columns.map(column => s"${column}_index")).setOutputCols(columns.map(column => s"${column}_onehot"))
-    stringIndexer //:+ oneHot
+    stringIndexer
+  }
+
+  def accuracyScore(df: DataFrame) = {
+    val dfR = df.select("label", "prediction")
+    dfR.show()
+    val rdd = dfR.rdd.map(row ⇒ (row.getInt(0).toDouble, row.getDouble(1)))
+    new RegressionMetrics(rdd).rootMeanSquaredError
   }
 }
